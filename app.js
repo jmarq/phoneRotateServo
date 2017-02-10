47b818dd7455c6b44aeeb4f587a0588ba9e21435
James Marquardt <marquardtjames@gmail.com>
2017-02-10 12:59:06 -0600

    Update app.js
    removed unnecessary logs, added comment

--- app.js
+++ app.js
@@ -15,17 +15,15 @@ board.on("ready", function() {
 //in board ready function, set up and start the socket server.  have server handle angle events from socket clients.
 io=require("socket.io").listen(8080);//when the board is ready we'll start the server
 io.sockets.on('connection', function (socket) {
-    socket.on('angle',function(data){
+    socket.on('angle',function(data){ //a client sent an angle event, move the servo
       gamma=data.rollAngle+70;
       if(gamma<35){gamma=35}
       else if(gamma>105){gamma=105}
-      console.log(gamma);
       servo.move(gamma);
     });
     socket.emit("hello",{message:"hi there this is from the node server"});
-      
-    console.log("connnnnection!");
-    socket.broadcast.emit("message",{message:"someone else connected"});
+    console.log("a client has connected!");
+    
   
 
 });
