caba742fe44b0a7a4e3f4bed2955186b5774eaa1
James Marquardt <marquardtjames@gmail.com>
2017-02-13 23:28:27 -0600

    Update app.js
    indentation, comment

--- app.js
+++ app.js
@@ -11,22 +11,19 @@ board.on("ready", function() {
     servo: servo
   });
 
-
-//in board ready function, set up and start the socket server.  have server handle angle events from socket clients.
-io=require("socket.io").listen(8080);//when the board is ready we'll start the server
-io.sockets.on('connection', function (socket) {
-    socket.on('angle',function(data){ //a client sent an angle event, move the servo
-      gamma=data.rollAngle+70;
-      if(gamma<35){gamma=35}
-      else if(gamma>105){gamma=105}
-      servo.move(gamma);
-    });
-    socket.emit("hello",{message:"hi there this is from the node server"});
-    console.log("a client has connected!");
-    
-  
-
-});
+  //in board ready function, set up and start the socket server.  have server handle angle events from socket clients.
+  io=require("socket.io").listen(8080);//when the board is ready we'll start the server
+  io.sockets.on('connection', function (socket) {
+      socket.on('angle',function(data){ //a client sent an angle event, move the servo
+        gamma=data.rollAngle+70;
+          // my servo operates best from 35 to 105
+        if(gamma<35){gamma=35}
+        else if(gamma>105){gamma=105}
+        servo.move(gamma);
+      });
+      socket.emit("hello",{message:"hi there this is from the node server"});
+      console.log("a client has connected!");
+  });
 
 });//end of board ready function
 
